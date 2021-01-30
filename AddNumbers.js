e881f49dc6e2c04a20e3fd9e94ea3e0686a8ae87
andersonIdo <gba18001@byui.edu>
2021-01-29 20:49:42 -0700

    Program4-1.html

---
+++ AddNumbers.js
@@ -0,0 +1,9 @@
+function addNumbers(){
+  let Number1 = parselFloat("10");
+  let Number2 = parselFloat("10");
+  // math
+  let addition = Number1 + 10
+  let addition =Number2 + 10
+
+  document.getElementById('output').innerHTML = addition;
+}
